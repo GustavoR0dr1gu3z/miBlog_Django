a4bcf830e33caef3b1d8869890cb840680dc99c5
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-01-26 21:36:14 -0600

    mensaje de views a html

--- miSitio/apps/miBlog/views.py
+++ miSitio/apps/miBlog/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request): #Creando una funcion que muestra lo que queremos en la pagina principal
     return render(request,'index.html',{
-        
+        'mensaje': 'Hecho Por Gustavo Calzada', #Informacion
     })
