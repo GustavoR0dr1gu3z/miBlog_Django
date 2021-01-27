210a882c949143f6ff194bade1c06e3ceff1152d
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-01-26 21:29:14 -0600

    creando archivo html

--- miSitio/apps/miBlog/views.py
+++ miSitio/apps/miBlog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-def index(request):
+def index(request): #Creando una funcion que muestra lo que queremos en la pagina principal
     return HttpResponse("<h1>Holaaa</h1>")
