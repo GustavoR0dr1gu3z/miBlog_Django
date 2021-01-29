c7933d241d61e80b1eb25e2e6167628dacf9d3cc
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-01-29 15:51:36 -0600

    creando el cierre de sesion del usuario

--- miSitio/apps/miBlog/views.py
+++ miSitio/apps/miBlog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse, redirect #4.1 .-  se importa redirect, para redireccionar a una pagina si el usuario existe
-from django.contrib.auth import authenticate, login #4.-  Se importa la autenticacion y el login
+from django.contrib.auth import authenticate, login, logout #4.-  Se importa la autenticacion y el login
 from django.contrib import messages #Para mostrar mensajes
 
 # Create your views here.
@@ -26,3 +26,17 @@ def ingresar(request): #2.- Ya que creamos la URL, creamos el metodo que le crea
     return render(request, 'login.html', {
     
     })    
+
+
+def salir(request): # Creando funcion que hace que terminemos la sesion
+    logout(request)
+    messages.success(request, 'Sesion Finalizada')
+    return redirect('ingresar')
+
+
+
+
+
+
+
+
--- miSitio/miSitio/urls.py
+++ miSitio/miSitio/urls.py
@@ -17,8 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from apps.miBlog import views # Importando la vista de miBlog
 
-urlpatterns = [
-    path('usuarios/ingresar',views.ingresar, name="ingresar"), # 1.- Crear URL, donde queremos que la ruta se ingrese 127.0.0.1:8000/usuarios/ingresar
+urlpatterns = [    
+    path('usuarios/salir',views.salir, name="salir"),
+    path('usuarios/ingresar',views.ingresar, name="ingresar"), # 1.- Crear URL, donde queremos que la ruta se ingrese 127.0.0.1:8000/usuarios/ingresar    
     path('',views.index, name="index"), # Llamar la funcion que se creo en views
     path('admin/', admin.site.urls),
 ]
