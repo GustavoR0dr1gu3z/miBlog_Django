0a599961a1b8959eff926544c238bffea5e4984a
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-01-29 12:28:30 -0600

    creando formulario Login

--- miSitio/apps/miBlog/views.py
+++ miSitio/apps/miBlog/views.py
@@ -5,3 +5,9 @@ def index(request): #Creando una funcion que muestra lo que queremos en la pagin
     return render(request,'index.html',{
         'mensaje': 'Hecho Por Gustavo Calzada', #Informacion
     })
+
+
+def ingresar(request): #2.- Ya que creamos la URL, creamos el metodo que le creamos desde URL, y recibe request y nuestro template (login.html)
+    return render(request, 'login.html', {
+
+    })    
