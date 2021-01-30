5d059b8619fc213d7a45bd4fb7f1a59c635f3393
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-01-29 20:49:57 -0600

    creando el diseño de registro usuarios

--- miSitio/apps/miBlog/forms.py
+++ miSitio/apps/miBlog/forms.py
@@ -1,10 +1,14 @@
 from django import forms # Importamos libreria para los formularios
 
 class RegistroForm(forms.Form): # Listos para crear los input del formulario
-    username = forms.CharField(required = True, min_length = 4, max_length=60)
-    email = forms.EmailField(required = True)
-    password = forms.CharField(required = True)
-    password2 = forms.CharField(required = True)
+    username = forms.CharField(required = True, min_length = 4, max_length=60, widget=forms.TextInput(attrs={'class': 'form-control',
+                                                                                                            'placeholder': 'Nombre de Usuario'}))
+    email = forms.EmailField(required = True, widget=forms.EmailInput(attrs={'class': 'form-control',
+                                                                            'placeholder': 'hola@ejemplo.com'}))
+    password = forms.CharField(required = True, widget=forms.PasswordInput(attrs={'class': 'form-control',
+                                                                                'placeholder': 'Password'}))
+    password2 = forms.CharField(required = True,  widget=forms.PasswordInput(attrs={'class': 'form-control',
+                                                                                'placeholder': 'Repeat Password'}))
 
 
 
