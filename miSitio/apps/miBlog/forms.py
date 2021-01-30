879fc8bdee38d21e22b4bed12b91f3ab62b77e90
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-01-29 21:00:48 -0600

    terminando el diseño del formulario

--- miSitio/apps/miBlog/forms.py
+++ miSitio/apps/miBlog/forms.py
@@ -1,13 +1,13 @@
 from django import forms # Importamos libreria para los formularios
 
 class RegistroForm(forms.Form): # Listos para crear los input del formulario
-    username = forms.CharField(required = True, min_length = 4, max_length=60, widget=forms.TextInput(attrs={'class': 'form-control',
+    username = forms.CharField(label = 'Nombre de Usuario', required = True, min_length = 4, max_length=60, widget=forms.TextInput(attrs={'class': 'form-control',
                                                                                                             'placeholder': 'Nombre de Usuario'}))
-    email = forms.EmailField(required = True, widget=forms.EmailInput(attrs={'class': 'form-control',
+    email = forms.EmailField(label = 'Correo Electrónico', required = True, widget=forms.EmailInput(attrs={'class': 'form-control',
                                                                             'placeholder': 'hola@ejemplo.com'}))
-    password = forms.CharField(required = True, widget=forms.PasswordInput(attrs={'class': 'form-control',
+    password = forms.CharField(label = 'Contraseña', required = True, widget=forms.PasswordInput(attrs={'class': 'form-control',
                                                                                 'placeholder': 'Password'}))
-    password2 = forms.CharField(required = True,  widget=forms.PasswordInput(attrs={'class': 'form-control',
+    password2 = forms.CharField(label = 'Repetir Contraseña', required = True,  widget=forms.PasswordInput(attrs={'class': 'form-control',
                                                                                 'placeholder': 'Repeat Password'}))
 
 
