d843f1b2fe3f3e55ea966f72cf2a840069a2026a
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-01-30 20:20:32 -0600

    validacion de usuarios

--- miSitio/apps/miBlog/forms.py
+++ miSitio/apps/miBlog/forms.py
@@ -1,4 +1,6 @@
 from django import forms # Importamos libreria para los formularios
+from django.contrib.auth.models import User # Clase por defecto que tiene Django
+
 
 class RegistroForm(forms.Form): # Listos para crear los input del formulario
     username = forms.CharField(label = 'Nombre de Usuario', required = True, min_length = 4, max_length=60, widget=forms.TextInput(attrs={'class': 'form-control',
@@ -9,6 +11,19 @@ class RegistroForm(forms.Form): # Listos para crear los input del formulario
                                                                                 'placeholder': 'Password'}))
     password2 = forms.CharField(label = 'Repetir Contraseña', required = True,  widget=forms.PasswordInput(attrs={'class': 'form-control',
                                                                                 'placeholder': 'Repeat Password'}))
+
+    def clean_username(self): # Metodo para validacion 
+        username = self.cleaned_data.get('username') # consulta para verificar si existe el nombre de usuario
+        if(User.objects.filter(username=username).exists()):
+            raise forms.ValidationError('Nombre de Usuario en Uso')
+        return username
+
+    def clean_email(self): # Metodo para validacion 
+        email = self.cleaned_data.get('email') # consulta para verificar si existe el nombre de usuario
+        if(User.objects.filter(email=email).exists()):
+            raise forms.ValidationError('Correo Electrónico en Uso')
+        return email
+
 
 
 
